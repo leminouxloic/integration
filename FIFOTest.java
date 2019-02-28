54b79fad8520e3f9d190af0e58b85abddf59c35d
loiclm <loic.lm17@gmail.com>
2019-02-28 10:54:04 +0100

    implémentation des classes de test

--- FIFOTest.java
+++ FIFOTest.java
@@ -1,14 +1,50 @@
 package integrationtpun;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Test;
 
 class FIFOTest {
 
 	@Test
-	public void test() throws Exception {
-		//Test à implémenter
+	void testSize() {
+		FIFO unFifo = new FIFO();
+		int expected = 1;
+		
+		unFifo.add(4);
+		
+		Assertions.assertEquals(expected, unFifo.size());
 	}
-
+	
+	@Test
+	void testFirst() {
+		FIFO unFifo = new FIFO();
+		Integer expected = 4;
+		
+		unFifo.add(4);
+		
+		Assertions.assertEquals(expected, unFifo.first());
+	}
+	
+	@Test
+	void testEmpty() {
+		FIFO unFifo = new FIFO();
+		boolean expected = true;
+		
+		
+		Assertions.assertEquals(expected, unFifo.isEmpty());
+	}
+	
+	@Test
+	void testRemove() {
+		FIFO unFifo = new FIFO();
+		int expected = 0;
+		
+		unFifo.add(4);
+		unFifo.removeFirst();
+		
+		Assertions.assertEquals(expected, unFifo.size());
+	}
 }
--- TriTableauTest.java
+++ TriTableauTest.java
@@ -7,8 +7,18 @@ import org.junit.jupiter.api.Test;
 class TriTableauTest {
 
 	@Test
-	public void test() throws Exception {
-		//Test à implémenter
+	public void testCroissant() throws Exception {
+		int tab[] = {2,4,5,6,8,1};
+		int valid[] = {1,2,4,5,6,8};
+		TriTableau.triCroissant(tab);
+		assertEquals(valid[0],tab[0]);
+	}
+	@Test
+	public void testDecroissant() throws Exception {
+		int tab[] = {2,4,5,6,8,1};
+		int valid[] = {8,6,5,4,2,1};
+		TriTableau.triDecroissant(tab);
+		assertEquals(valid[0],tab[0]);
 	}
 
 }
